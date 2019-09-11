36b86bddbea64458e1cd5aec15d3edcd51b14446
surega21 <surega@amazon.com>
2019-09-11 15:14:13 -0700

    Added ListHandler for ReceiptFilter (#18)
    * Added ListHandler for ReceiptFilter
    * Added translator to convert SDK models into handler models

--- aws-ses-receiptfilter/src/main/java/com/amazonaws/ses/receiptfilter/ListHandler.java
+++ aws-ses-receiptfilter/src/main/java/com/amazonaws/ses/receiptfilter/ListHandler.java
@@ -1,30 +1,38 @@
 package com.amazonaws.ses.receiptfilter;
 
-import com.amazonaws.cloudformation.proxy.AmazonWebServicesClientProxy;
-import com.amazonaws.cloudformation.proxy.Logger;
-import com.amazonaws.cloudformation.proxy.ProgressEvent;
-import com.amazonaws.cloudformation.proxy.OperationStatus;
-import com.amazonaws.cloudformation.proxy.ResourceHandlerRequest;
+import com.amazonaws.cloudformation.proxy.*;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.ListReceiptFiltersRequest;
+import software.amazon.awssdk.services.ses.model.ListReceiptFiltersResponse;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListHandler extends BaseHandler<CallbackContext> {
+    private AmazonWebServicesClientProxy proxy;
+    private SesClient client = ClientBuilder.getClient();
 
     @Override
     public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
-        final ResourceHandlerRequest<ResourceModel> request,
-        final CallbackContext callbackContext,
-        final Logger logger) {
-
-        final List<ResourceModel> models = new ArrayList<>();
-
-        // TODO : put your code here
-
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final Logger logger) {
+        this.proxy = proxy;
+        final List<ResourceModel> models = listReceiptFilters();
         return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModels(models)
-            .status(OperationStatus.SUCCESS)
-            .build();
+                .resourceModels(models)
+                .status(OperationStatus.SUCCESS)
+                .build();
+    }
+
+    private List<ResourceModel> listReceiptFilters() {
+        /*  API documentation : https://docs.aws.amazon.com/ses/latest/APIReference/API_ListReceiptFilters.html
+        This API does not support pagination */
+        final ListReceiptFiltersResponse response = this.proxy.injectCredentialsAndInvokeV2(ListReceiptFiltersRequest.builder().build(), this.client::listReceiptFilters);
+        return response.filters()
+                .stream()
+                .map(f -> ResourceModel.builder().filter(Translator.translate(f)).build())
+                .collect(Collectors.toList());
     }
 }
