475409f0e868d7087fb40f63a645da9307849c0c
Archana Suregaonkar <surega@amazon.com>
2019-10-18 16:47:59 -0700

    Fix code coverage

--- aws-ses-receiptfilter/src/test/java/com/amazonaws/ses/receiptfilter/TranslatorTest.java
+++ aws-ses-receiptfilter/src/test/java/com/amazonaws/ses/receiptfilter/TranslatorTest.java
@@ -7,19 +7,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TranslatorTest {
     @Test
-    public void test_translate_ReceiptIpFilter(){
+    public void test_translate_ReceiptIpFilter() {
         final software.amazon.awssdk.services.ses.model.ReceiptIpFilter in = software.amazon.awssdk.services.ses.model.ReceiptIpFilter.builder()
                 .cidr("10.0.0.1/24")
                 .policy("Allow")
                 .build();
-        final com.amazonaws.ses.receiptfilter.IpFilter out = Translator.translate(in);
-        final software.amazon.awssdk.services.ses.model.ReceiptIpFilter result  = Translator.translate(out);
+        IpFilter out = Translator.translate(in);
+        final software.amazon.awssdk.services.ses.model.ReceiptIpFilter result = Translator.translate(out);
         assertThat(out).isNotNull();
         assertThat(in).isEqualTo(result);
+
+        out = Translator.translate((software.amazon.awssdk.services.ses.model.ReceiptIpFilter) null);
+        assertThat(out).isNull();
     }
 
     @Test
-    public void test_translate_ReceiptFilter(){
+    public void test_translate_ReceiptFilter() {
         final software.amazon.awssdk.services.ses.model.ReceiptFilter in = software.amazon.awssdk.services.ses.model.ReceiptFilter.builder()
                 .ipFilter(software.amazon.awssdk.services.ses.model.ReceiptIpFilter.builder()
                         .cidr("10.0.0.1/24")
@@ -27,9 +30,45 @@ public class TranslatorTest {
                         .build())
                 .name("test")
                 .build();
-        final com.amazonaws.ses.receiptfilter.Filter out = Translator.translate(in);
+        Filter out = Translator.translate(in);
         final software.amazon.awssdk.services.ses.model.ReceiptFilter result = Translator.translate(out);
         assertThat(out).isNotNull();
         assertThat(in).isEqualTo(result);
+
+        out = Translator.translate((software.amazon.awssdk.services.ses.model.ReceiptFilter) null);
+        assertThat(out).isNull();
+    }
+
+    @Test
+    public void test_translate_IpFilter() {
+        final IpFilter in = IpFilter.builder()
+                .cidr("10.0.0.1/24")
+                .policy("Allow")
+                .build();
+        software.amazon.awssdk.services.ses.model.ReceiptIpFilter out = Translator.translate(in);
+        final IpFilter result = Translator.translate(out);
+        assertThat(out).isNotNull();
+        assertThat(in).isEqualTo(result);
+
+        out = Translator.translate((IpFilter) null);
+        assertThat(out).isNull();
+    }
+
+    @Test
+    public void test_translate_Filter() {
+        final Filter in = Filter.builder()
+                .ipFilter(IpFilter.builder()
+                        .cidr("10.0.0.1/24")
+                        .policy("Allow")
+                        .build())
+                .name("test")
+                .build();
+        software.amazon.awssdk.services.ses.model.ReceiptFilter out = Translator.translate(in);
+        final Filter result = Translator.translate(out);
+        assertThat(out).isNotNull();
+        assertThat(in).isEqualTo(result);
+
+        out = Translator.translate((Filter) null);
+        assertThat(out).isNull();
     }
 }
