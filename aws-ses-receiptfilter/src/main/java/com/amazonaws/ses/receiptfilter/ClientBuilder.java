8df12b1bff9ad8c92c3994bd84f769f8e5ee725b
Wael Abdelrahman <waelab@amazon.com>
2019-11-14 15:56:49 -0800

    Delete receiptfilter and ConfigurationSetEventDestination resources
    Delete receiptfilter and ConfigurationSetEventDestination resources

--- aws-ses-receiptfilter/src/main/java/com/amazonaws/ses/receiptfilter/ClientBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.amazonaws.ses.receiptfilter;
-
-import software.amazon.awssdk.services.ses.SesClient;
-
-public class ClientBuilder {
-    public static SesClient getClient() {
-        return SesClient.builder().build();
-    }
-}
